bf0fa4951f713dd98223ce4979a5996bc3517bdb
Typezera <willianbardela2131@hotmail.com>
2026-05-03 13:36:25 -0300

    documentation: documentação dos endpoints das tarefas

--- src/main/java/com/loginComJwt/loginJWT/controller/task/TaskController.java
+++ src/main/java/com/loginComJwt/loginJWT/controller/task/TaskController.java
@@ -5,6 +5,7 @@ import com.loginComJwt.loginJWT.dto.taskDto.TaskResponseDTO;
 import com.loginComJwt.loginJWT.dto.taskDto.dtoPatch.updateTaskRequestDTO;
 import com.loginComJwt.loginJWT.dto.taskDto.dtoPatch.updateTaskResponseDTO;
 import com.loginComJwt.loginJWT.service.task.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,17 +21,29 @@ public class TaskController {
 
     public TaskController(TaskService taskService){this.taskService = taskService;}
 
+    @Operation(
+            summary = "Trás todas as tarefas do usuário logado.",
+            description = "Exibe apenas as tarefas que pertencem a aquela usuário logado."
+    )
     @GetMapping()// trás as tarefas do proprio usuário
     public ResponseEntity<List<TaskResponseDTO>> verTarefas(){
         return ResponseEntity.ok(taskService.mostrarTarefas());
     }
 
+    @Operation(
+            summary = "cria uma tarefa.",
+            description = "Apenas um usuário logado pode criar uma tarefa."
+    )
     @PostMapping("/create")
     public ResponseEntity<TaskResponseDTO> criarTarefas(
             @Valid @RequestBody TaskRequestDTO taskRequestDTO){
         return ResponseEntity.ok(taskService.criarTarefa(taskRequestDTO));
     };
 
+    @Operation(
+            summary = "Atualzia a descrição da tarefa.",
+            description = "Apenas o dono da tarefa pode atualizar."
+    )
     @PatchMapping("/update/{id}")
     public ResponseEntity<updateTaskResponseDTO> updateDescricao(
             @Valid
@@ -39,6 +52,10 @@ public class TaskController {
         return ResponseEntity.ok(taskService.atualizarDescricaoTarefa(id, descricao));
     }
 
+    @Operation(
+            summary = "Deleta uma tarefa pelo ID",
+            description = "É possivel remover uma tarefa pelo ID, apenas o dono da tarefa pode remove-la"
+    )
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> removerTarefa(
             @PathVariable Long id
